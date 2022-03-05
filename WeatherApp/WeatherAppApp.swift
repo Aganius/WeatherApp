7eeb5f56b3a8147a0bbe4941fe9ea9aaed35727b
Elioth Quintana <elioth@moxehub.com>
2022-03-05 11:20:49 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Elioth Quintana on 5/03/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
